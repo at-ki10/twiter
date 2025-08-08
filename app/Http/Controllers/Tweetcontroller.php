89dedbab9dac7f8ed3e3146f0eb02157338153f7
Turki <trkylalshry74@gmail.com>
2025-08-08 19:39:10 +0300

    fix

--- app/Http/Controllers/Tweetcontroller.php
+++ app/Http/Controllers/Tweetcontroller.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Auth;
 class TweetController extends Controller
 {
     function index()
-    {
+ {
         $tweets = Tweet::query()
         ->where('parent_tweet_id',null)
             ->orderByDesc('created_at')
